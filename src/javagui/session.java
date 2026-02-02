7acfd54793e3c1fb14cd7b9f569d20df6b051c0d
Train-Of-Thought <zyravanes@gmail.com>
2026-02-02 19:13:39 +0800

    Design pls

---
+++ src/javagui/session.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javagui;
+
+/**
+ *
+ * @author Zyriel
+ */
+public class session {
+    public static int userId;
+     public static String userType;
+}
